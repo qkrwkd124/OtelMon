1f742a31a00eb93f40fa9166853e25bcb45f8639
qkrwkd124 <qkrwkd120@naver.com>
2025-02-18 23:02:19 +0900

    otlp 사용

--- src/monitoring/main.py
+++ src/monitoring/main.py
@@ -1,11 +1,11 @@
 import os
-from src.monitoring.telmetry_config import init_telemetry
+from src.monitoring.telmetry_config import init_telemetry, init_telemetry_otlp
 from src.etl.etl_process import etl_process
 
 
 
 def main():
-    init_telemetry()
+    init_telemetry_otlp()
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     csv_path = os.path.join(current_dir, "../data/data.csv")
