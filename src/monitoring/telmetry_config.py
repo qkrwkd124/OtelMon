0bcdfac55940a951630be319d29dd09c2e3018ba
qkrwkd124 <qkrwkd120@naver.com>
2025-02-19 23:24:36 +0900

    tarcer, metric 추가

--- src/monitoring/telmetry_config.py
+++ src/monitoring/telmetry_config.py
@@ -6,6 +6,9 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExport
 from opentelemetry.trace import Status, StatusCode
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry import metrics
 
 def init_telemetry():
 
@@ -38,7 +41,7 @@ def init_telemetry_otlp():
     otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317/v1/traces")
     
     # BatchSpanProcessor 에 OTLPSpanExporter 등록
-    span_processor = BatchSpanProcessor(otlp_exporter)``
+    span_processor = BatchSpanProcessor(otlp_exporter)
     provider.add_span_processor(span_processor)
     
     # 트레이서 프로바이더 설정
@@ -47,3 +50,38 @@ def init_telemetry_otlp():
     return provider
 
 
+def init_tracer():
+    """
+    OpenTelemetry 트레이서 초기화
+    """
+    resoure = Resource.create(attributes={"service.name": "etl_tracer", "service.version": "1.0.0","host.name": os.uname().nodename})
+    
+    tracer_provider = TracerProvider(resource=resoure)
+    
+    span_processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://localhost:4317/v1/traces"))
+    
+    tracer_provider.add_span_processor(span_processor)
+    
+    trace.set_tracer_provider(tracer_provider)
+    
+    return trace.get_tracer(__name__)
+
+def init_meter():
+    """
+    OpenTelemetry 메트릭 초기화
+
+    Returns:
+        MeterProvider: 메트릭 프로바이더
+    """
+    
+    otlp_metric_exporter = OTLPMetricExporter(endpoint="http://localhost:4317/v1/metrics")
+    
+    # 주기적으로 메트릭 데이터를 내보내는 MetricReader 생성
+    metric_reader = PeriodicExportingMetricReader(otlp_metric_exporter)
+    
+    # MeterProvider 생성
+    provider = MeterProvider(metric_readers=[metric_reader])
+    
+    # 메트릭 프로바이더 설정
+    
+    return provider.get_meter(__name__)
