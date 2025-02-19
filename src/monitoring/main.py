48b69422674657a8ee66ef18ced0a65fa1c1397c
qkrwkd124 <qkrwkd120@naver.com>
2025-02-19 23:24:17 +0900

    csv_process 실행 코드 추가

--- src/monitoring/main.py
+++ src/monitoring/main.py
@@ -1,7 +1,7 @@
 import os
-from src.monitoring.telmetry_config import init_telemetry, init_telemetry_otlp
+from src.monitoring.telmetry_config import init_telemetry, init_telemetry_otlp, init_meter, init_tracer
 from src.etl.etl_process import etl_process
-
+from src.etl.csv_process import csv_process
 
 
 def main():
@@ -11,6 +11,17 @@ def main():
     csv_path = os.path.join(current_dir, "../data/data.csv")
 
     etl_process(csv_path)
+    
+def main2():
+    
+    meter = init_meter()
+    tracer = init_tracer()
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(current_dir, "../data/data.csv")
+    
+    csv_process(csv_path, tracer, meter)
+    print("작업 완료")
 
 if __name__ == "__main__":
-    main()
+    main2()
