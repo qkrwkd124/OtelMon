b915e78f87e2be04064ef6645e9153e17ace50cc
qkrwkd124 <qkrwkd120@naver.com>
2025-04-14 15:11:00 +0900

    컬럼추가가

--- airflow/dags/sql/create_alert_history.sql
+++ airflow/dags/sql/create_alert_history.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS alert_history (
     id INT AUTO_INCREMENT PRIMARY KEY,
     process_execution_id INT NOT NULL UNIQUE,
+    platform_type VARCHAR(200),
+    group_name VARCHAR(200),
     process_name VARCHAR(200),
     end_time DATETIME,
     alert_sent_at DATETIME DEFAULT CURRENT_TIMESTAMP
